2cfaf7ca0beaba2c0a70d44c59d26af38a48ed55
Guilherme Jacinto <guilherme.jacinto@outlook.com.br>
2022-05-23 19:33:06 -0300

    Adicionado texto no Header e criação de três botões

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -20,6 +20,29 @@ function Header() {
                        >Login</Button>
             </span>
         </div>
+        <div classname='texto'>
+                <h1>Para quem almeja o sucesso!</h1>
+                    <h2>Nós entregamos.</h2>
+        </div>
+              <div className='rodape'>
+                    <span className='opcoes'>
+                        <Button
+                               className="BtnSolicitarEntrega"
+                               variant="contained"
+                               margin={"normal"}
+                               > Solicitar Entrega</Button>
+                        <Button
+                               className="BtnAcompanharEnvio"
+                               variant="contained"
+                               margin={"normal"}
+                               >Acompanhar Envio</Button>
+                        <Button
+                               className="BtnContratarEntregador"
+                               variant="contained"
+                               margin={"normal"}
+                               >Contratar Entregador</Button>
+                    </span>
+                </div>
       </header>
     );
 }
