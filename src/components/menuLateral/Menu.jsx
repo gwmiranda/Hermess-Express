d432c009f341aedb8d7b7b13dd46524d104d3789
Gabriel Senna <gabrielssimao2000@gmail.com>
2022-05-30 17:53:21 -0300

    feat: engordando os botoes

--- src/components/menuLateral/Menu.jsx
+++ src/components/menuLateral/Menu.jsx
@@ -49,10 +49,11 @@ const Menu = () => {
                 fontSize: 25,
                 background: '#fff',
                 textTransform: 'capitalize',
-                padding: 7,
+                paddingLeft: 20,
+                paddingRight: 20,
                 borderRadius: 20,
-                marginLeft: 15,
-                marginRight: 15,
+                marginLeft: 10,
+                marginRight: 10,
               }}
             >
               Acompanhar Rastreio
@@ -64,10 +65,11 @@ const Menu = () => {
                 fontSize: 25,
                 background: '#fff',
                 textTransform: 'capitalize',
-                padding: 7,
+                paddingLeft: 20,
+                paddingRight: 20,
                 borderRadius: 20,
-                marginLeft: 15,
-                marginRight: 15,
+                marginLeft: 10,
+                marginRight: 10,
               }}
             >
               Simulação
