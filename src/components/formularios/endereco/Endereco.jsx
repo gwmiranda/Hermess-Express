ed12524334aeaeabeacd0ab895a9d345e630776d
Guilherme Miranda <gwdemiranda@outlook.com>
2022-05-30 21:40:59 -0300

    Criando formulario Login

--- src/components/formularios/endereco/Endereco.jsx
+++ src/components/formularios/endereco/Endereco.jsx
@@ -1,63 +1,27 @@
-import './Login.css';
 import {Button, Card, FormControl, TextField} from "@mui/material";
-import axios from "../../axios";
-import {useState} from "react";
 
-function Login() {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    let token = '';
-
-    async function loginRequest() {
-        axios.post('auth/login',{
-            email: email,
-            password : password
-
-        }).then((res) => {
-            token = res.data;
-            console.log(res.data)
-
-        }).catch((error) => {
-            console.log(error.response)
-            alert(error)
-        })
-    }
-
+function Endereco() {
     return (
         <div>
-            <header className={"header"}>
-                <h1>Hermes Express</h1>
-            </header>
             <Card className= "card">
                 <form
                     className="form"
                     onSubmit={event => {
-                        loginRequest()
+                        console.log("Login")
+                        event.preventDefault();
                     }}
                 >
                     <h2>Login</h2>
                     <FormControl className="formComponent">
                         <TextField
-                            value={email}
-                            onChange={event => {
-                                setEmail(event.target.value)
-                            }}
-                            name="email"
-                            id="email"
-                            label="Email"
+                            id="usuario"
+                            label="Usuário"
                             variant="outlined"
                             fullWidth
                             required
                             margin={"normal"}
                         />
                         <TextField
-                            value={password}
-                            onChange={event => {
-                                setPassword(event.target.value)
-                            }}
-                            name="senha"
                             id="Senha"
                             type="password"
                             label="Senha"
@@ -79,7 +43,6 @@ function Login() {
                             variant="contained"
                             margin={"normal"}
                             color={"error"}
-                            onClick={onsubmit}
                         >Registrar</Button>
                     </FormControl>
                 </form>
@@ -88,4 +51,4 @@ function Login() {
     );
 }
 
-export default Login;
+export default Endereco;
