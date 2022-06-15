9c8b5a8b745a50fb9bea225629b85d600b985187
Guilherme Miranda <gwdemiranda@outlook.com>
2022-06-15 08:59:44 -0300

    Criando gráficos

--- src/components/graficos/GraficoPizza.jsx
+++ src/components/graficos/GraficoPizza.jsx
@@ -1,17 +1,41 @@
-import {useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import { Chart } from "react-google-charts";
+import DataContext from "../../data/DataContext";
+import axios from "../../axios";
 
 const GraficoPizza = (props) => {
 
+    const {state, setState} = useContext(DataContext)
+
     const [options, setOptions] = useState({
-        title: 'Gráfico de Pizza'
+        title: 'Encomendas por tipo de entregas'
     })
     const [data, setData] = useState([
-        ['Linguagens', 'Quantidade'],
-        ['React', 100],
-        ['Angula', 80],
-        ['Vue', 50],
+        ['Tipo Entrega', 'Quantidade'],
+        ['Hermes Express', 0],
+        ['Hermes Standard', 0],
+        ['Hermes Tork', 0],
     ])
+
+    useEffect(() => {
+        response();
+    }, [])
+
+    const response = async () => {
+        return axios.get(`/package/getGroupByUserId/${state.id}`)
+            .then((res) => {
+                setData([
+                    ['Tipo Entrega', 'Quantidade'],
+                    ['Hermes Express', res.data.data.encomenda[1].HermesExpress],
+                    ['Hermes Standard', res.data.data.encomenda[2].HermesStandard],
+                    ['Hermes Tork', res.data.data.encomenda[3].HermesTork],
+                ])
+            })
+            .catch((error) => {
+                console.log(error.response);
+                alert(error);
+            })
+    }
 
     return (
         <>
